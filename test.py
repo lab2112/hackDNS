ea925d83ea846cdfb6f0925aab18ce69b720b84a
lab2112 <brandon.coates@gmail.com>
2014-11-01 18:18:31 -0400

    updates to noisyDNS and test file
    noisyDNS: erased extra lines
    
    test file: putting it in normal python format and attempting to sniff
    traffic

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
 from scapy.all import *
 
 #testfile to try things out
-sniff_filter='udp port 53'
+sniff_filter='tcp port 80'
 
 def sendfunc():
-	ip = IP(src="1.1.1.1", dst="2.2.2.2")
-
-sniff(filter=sniff_filter, prn=sendfunc())
+	#ip = IP(src="1.1.1.1", dst="2.2.2.2")
+	print "Caught a packet"
+def main():
+	print "starting the sniffer for UDP packets"
+	a=sniff(prn=sendfunc())
+	print a 
+if __name__=='__main__':
+	main()
