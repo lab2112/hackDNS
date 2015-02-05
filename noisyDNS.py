0532ff3f449bc94bbd8b3c1af7d1fc0ff7cdec77
lab2112 <brandon.coates@gmail.com>
2015-02-05 03:02:23 -0500

    Updating args and sniff

--- noisyDNS.py
+++ noisyDNS.py
@@ -2,18 +2,18 @@
 from scapy.all import *
 import argparse
 
-'''
-parser = argparse.ArgumentParser()
-parser.add_argument("targetsite",  help="the webiste that your are faking the DNS repsonse")
-parser.add_argument("evilIP", help="IP of evil website")
-parser.add_argument("dnsserver", help="spoofed DNS server")
-parser.add_argument("target", help="your target")
-args = parser.parse_args()
-'''
 
 #sniff the traffic for at least a minute to try to capture DNS server
-def sniff_dns():
-	print "filler"
+def sniff_dns(intime):
+	print "sniffing for DNS requests"
+	dns_filter = "udp port 53"
+	sniff_time = 60
+		
+	sniff(filter=dns_filter, prn=announce(pkt), time=sniff_time)
+
+#proud announcement and packet breakdown
+def announce(pkt):
+	print "got one"
 
 #spam client with DNS replies
 def spam_replies():
@@ -24,8 +24,20 @@ def surgical_strike():
 	print "filler"
 
 def main():
-	#Args -w <website>, -e <IP of site to redirect user>, -d <DNS Server if known>
+	#Args -e <IP of site to redirect user>, -d <DNS Server if known>
 	# -t <target>, -n <noisy>
+	parser = argparse.ArgumentParser(description='Some fun stuff with DNS')
+
+	parser.add_argument("-e", "--evil", help="ip of website to redirect user")
+	parser.add_argument("-d", "--DNS", help="DNS Server of target, if known")
+	parser.add_argument("-n", "--noisy", help="Spam the target with responses")
+	parser.add_argument("-s", "--secs", help="the amount of spent sniffing")
+	parser.add_argument("-t", "--target", help="the target IP")
+	args = parser.parse_args()
+
+	#print args
+	sniff_dns(args.secs)
+
 
 
 if __name__=="__main__":
