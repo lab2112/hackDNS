689a3cbd4c35f3e21471f86e722e244d9a081f53
lab2112 <brandon.coates@gmail.com>
2015-02-13 02:13:19 -0500

    Started to tear down packets

--- noisyDNS.py
+++ noisyDNS.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 from scapy.all import *
 import argparse
-
+import thread
 
 #sniff the traffic for at least a minute to try to capture DNS server
 def sniff_dns(sniff_time):
 	print "sniffing for DNS requests"
-	dns_filter = "udp port 53"
+	dns_filter = "dst port 53"
 
 	#checking time to run the filter
 	if sniff_time==None:
@@ -14,11 +14,13 @@ def sniff_dns(sniff_time):
 	else:
 		sniff_time = int(sniff_time)
 
-	sniff(prn=attack, store=1, timeout=sniff_time)
+	sniff(prn=attack, store=1, timeout=sniff_time, filter=dns_filter)
 	
-#proud announcement and packet breakdown
+#packet breakdown
 def attack(packet):
-	print packet.summary()
+	a=packet
+	#debug statement to get names of fields
+	print ls(a)
 
 #spam client with DNS replies
 def spam_replies(evil_ip, target):
@@ -33,6 +35,7 @@ def surgical_strike():
 def main():
 	#Args -e <IP of site to redirect user>, -d <DNS Server if known>
 	# -t <target>, -n <noisy>
+	
 	parser = argparse.ArgumentParser(description='Some fun stuff with DNS')
 
 	parser.add_argument("-e", "--evil", help="ip of website to redirect user")
@@ -41,7 +44,7 @@ def main():
 	parser.add_argument("-s", "--secs", help="the amount of spent sniffing")
 	parser.add_argument("-t", "--target", help="the target IP")
 	args = parser.parse_args()
-
+	
 	
 	sniff_dns(args.secs)
 	#if args.noisy != None:
